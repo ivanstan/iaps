a23665375a20db6da9282f58c964445789c8aeb9
Ivan Stanojevic <ivanstan@gmail.com>
2021-06-28 20:50:10 +0200

    add map raster

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Map } from './components/Map';
 
-function App() {
-  return (
-    <Map containerElement={<div/>}
-         mapElement={<div style={{ height: window.innerHeight - 15, width: window.innerWidth -15 }}/>}
-         googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyCoPqCC8XT2kwShwfCms3xk6JE2d-LT9_o&libraries=visualization'}
-         loadingElement={<div style={{ height: `100%` }}/>}
-    />
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Map containerElement={<div/>}
+           mapElement={<div style={{ height: window.innerHeight - 15, width: window.innerWidth - 15 }}/>}
+           googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyCoPqCC8XT2kwShwfCms3xk6JE2d-LT9_o&libraries=visualization'}
+           loadingElement={<div style={{ height: `100%` }}/>}
+      />
+    );
+  }
 }
 
 export default App;
