21d9a7842d4b53dc7433110166320254cde853a6
Ivan Stanojevic <ivanstan@gmail.com>
2021-07-21 11:01:28 +0200

    fix folder structure

--- src/Kernel.php
+++ src/Kernel.php
@@ -13,26 +13,26 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import('../etc/{packages}/*.yaml');
+        $container->import('../etc/{packages}/'.$this->environment.'/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
-            $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
+        if (is_file(\dirname(__DIR__).'/etc/services.yaml')) {
+            $container->import('../etc/services.yaml');
+            $container->import('../etc/{services}_'.$this->environment.'.yaml');
         } else {
-            $container->import('../config/{services}.php');
+            $container->import('../etc/{services}.php');
         }
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
+        $routes->import('../etc/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../etc/{routes}/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
+        if (is_file(\dirname(__DIR__).'/etc/routes.yaml')) {
+            $routes->import('../etc/routes.yaml');
         } else {
-            $routes->import('../config/{routes}.php');
+            $routes->import('../etc/{routes}.php');
         }
     }
 }
