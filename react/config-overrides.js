24b86370b38b31ee6edc9135dcc9e52ccec34de4
Ivan Stanojevic <ivanstan@gmail.com>
2021-07-21 10:45:47 +0200

    change react public folder to static folder

--- react/config-overrides.js
+++ react/config-overrides.js
@@ -5,6 +5,8 @@ module.exports = {
     paths.appIndexJs = path.resolve(__dirname, 'client/index.tsx');
     paths.appSrc = path.resolve(__dirname, 'client');
     paths.appTypeDeclarations = path.resolve(__dirname, 'client/react-app-env.d.ts');
+    paths.appHtml = path.resolve(__dirname, 'static/index.html');
+    paths.appPublic = path.resolve(__dirname, 'static');
 
     return paths;
   },
