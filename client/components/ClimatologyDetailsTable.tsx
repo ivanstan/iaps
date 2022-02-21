c521f880fc74b06b48a9f586bb5d1078b6148606
Ivan Stanojevic <ivanstan@gmail.com>
2022-02-21 18:32:20 +0100

    optimizations

--- client/components/ClimatologyDetailsTable.tsx
+++ client/components/ClimatologyDetailsTable.tsx
@@ -38,129 +38,129 @@ export class ClimatologyDetailsTable extends React.Component<any, any> {
         <tbody>
         <tr>
           <th>Januar</th>
-          <td>{value['avg_jan']}</td>
-          <td>{value['med_jan']}</td>
-          <td>{value['p25_jan']}</td>
-          <td>{value['p75_jan']}</td>
-          <td>{value['p10_jan']}</td>
-          <td>{value['p90_jan']}</td>
+          <td>{parseFloat(value['avg_jan']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_jan']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_jan']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_jan']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_jan']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_jan']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Februar</th>
-          <td>{value['avg_feb']}</td>
-          <td>{value['med_feb']}</td>
-          <td>{value['p25_feb']}</td>
-          <td>{value['p75_feb']}</td>
-          <td>{value['p10_feb']}</td>
-          <td>{value['p90_feb']}</td>
+          <td>{parseFloat(value['avg_feb']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_feb']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_feb']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_feb']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_feb']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_feb']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Mart</th>
-          <td>{value['avg_mar']}</td>
-          <td>{value['med_mar']}</td>
-          <td>{value['p25_mar']}</td>
-          <td>{value['p75_mar']}</td>
-          <td>{value['p10_mar']}</td>
-          <td>{value['p90_mar']}</td>
+          <td>{parseFloat(value['avg_mar']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_mar']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_mar']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_mar']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_mar']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_mar']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>April</th>
-          <td>{value['avg_apr']}</td>
-          <td>{value['med_apr']}</td>
-          <td>{value['p25_apr']}</td>
-          <td>{value['p75_apr']}</td>
-          <td>{value['p10_apr']}</td>
-          <td>{value['p90_apr']}</td>
+          <td>{parseFloat(value['avg_apr']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_apr']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_apr']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_apr']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_apr']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_apr']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Maj</th>
-          <td>{value['avg_may']}</td>
-          <td>{value['med_may']}</td>
-          <td>{value['p25_may']}</td>
-          <td>{value['p75_may']}</td>
-          <td>{value['p10_may']}</td>
-          <td>{value['p90_may']}</td>
+          <td>{parseFloat(value['avg_may']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_may']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_may']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_may']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_may']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_may']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Jun</th>
-          <td>{value['avg_jun']}</td>
-          <td>{value['med_jun']}</td>
-          <td>{value['p25_jun']}</td>
-          <td>{value['p75_jun']}</td>
-          <td>{value['p10_jun']}</td>
-          <td>{value['p90_jun']}</td>
+          <td>{parseFloat(value['avg_jun']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_jun']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_jun']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_jun']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_jun']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_jun']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Jul</th>
-          <td>{value['avg_jul']}</td>
-          <td>{value['med_jul']}</td>
-          <td>{value['p25_jul']}</td>
-          <td>{value['p75_jul']}</td>
-          <td>{value['p10_jul']}</td>
-          <td>{value['p90_jul']}</td>
+          <td>{parseFloat(value['avg_jul']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_jul']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_jul']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_jul']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_jul']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_jul']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Avgust</th>
-          <td>{value['avg_avg']}</td>
-          <td>{value['med_avg']}</td>
-          <td>{value['p25_avg']}</td>
-          <td>{value['p75_avg']}</td>
-          <td>{value['p10_avg']}</td>
-          <td>{value['p90_avg']}</td>
+          <td>{parseFloat(value['avg_avg']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_avg']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_avg']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_avg']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_avg']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_avg']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Septembar</th>
-          <td>{value['avg_sep']}</td>
-          <td>{value['med_sep']}</td>
-          <td>{value['p25_sep']}</td>
-          <td>{value['p75_sep']}</td>
-          <td>{value['p10_sep']}</td>
-          <td>{value['p90_sep']}</td>
+          <td>{parseFloat(value['avg_sep']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_sep']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_sep']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_sep']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_sep']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_sep']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Oktobar</th>
-          <td>{value['avg_oct']}</td>
-          <td>{value['med_oct']}</td>
-          <td>{value['p25_oct']}</td>
-          <td>{value['p75_oct']}</td>
-          <td>{value['p10_oct']}</td>
-          <td>{value['p90_oct']}</td>
+          <td>{parseFloat(value['avg_oct']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_oct']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_oct']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_oct']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_oct']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_oct']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Novembar</th>
-          <td>{value['avg_nov']}</td>
-          <td>{value['med_nov']}</td>
-          <td>{value['p25_nov']}</td>
-          <td>{value['p75_nov']}</td>
-          <td>{value['p10_nov']}</td>
-          <td>{value['p90_nov']}</td>
+          <td>{parseFloat(value['avg_nov']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_nov']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_nov']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_nov']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_nov']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_nov']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Decembar</th>
-          <td>{value['avg_dec']}</td>
-          <td>{value['med_dec']}</td>
-          <td>{value['p25_dec']}</td>
-          <td>{value['p75_dec']}</td>
-          <td>{value['p10_dec']}</td>
-          <td>{value['p90_dec']}</td>
+          <td>{parseFloat(value['avg_dec']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_dec']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_dec']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_dec']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_dec']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_dec']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Vegetacija*</th>
-          <td>{value['avg_vegetation']}</td>
-          <td>{value['med_vegetation']}</td>
-          <td>{value['p25_vegetation']}</td>
-          <td>{value['p75_vegetation']}</td>
-          <td>{value['p10_vegetation']}</td>
-          <td>{value['p90_vegetation']}</td>
+          <td>{parseFloat(value['avg_vegetation']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_vegetation']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_vegetation']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_vegetation']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_vegetation']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_vegetation']).toFixed(1)}</td>
         </tr>
         <tr>
           <th>Godina</th>
-          <td>{value['avg_year']}</td>
-          <td>{value['med_year']}</td>
-          <td>{value['p25_year']}</td>
-          <td>{value['p75_year']}</td>
-          <td>{value['p10_year']}</td>
-          <td>{value['p90_year']}</td>
+          <td>{parseFloat(value['avg_year']).toFixed(1)}</td>
+          <td>{parseFloat(value['med_year']).toFixed(1)}</td>
+          <td>{parseFloat(value['p25_year']).toFixed(1)}</td>
+          <td>{parseFloat(value['p75_year']).toFixed(1)}</td>
+          <td>{parseFloat(value['p10_year']).toFixed(1)}</td>
+          <td>{parseFloat(value['p90_year']).toFixed(1)}</td>
         </tr>
         </tbody>
       </Table>
